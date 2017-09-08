929e8c804feb84c4d2089c59ef2bab5bfde76610
sidewinder2020 <sdwinder1@gmail.com>
2017-09-08 15:14:56 -0600

    Adjust routes, add update, edit, destroy to nest

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   resources :users, only: [:new, :create, :show] do
-    resources :stories, only: [:new, :create, :index, :destroy]
+    resources :stories, only: [:new, :create, :index, :destroy, :edit, :update]
   end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
