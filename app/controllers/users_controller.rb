da78c415cc5fecd1c84d2b8dfc68beb42d5928ae
sidewinder2020 <sdwinder1@gmail.com>
2017-09-08 14:12:04 -0600

    Create User Controller - need to refactor to combine stories/user in user @some point

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
+  before_action :require_login, except: [:new, :create]
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
@@ -14,4 +23,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :password, :picture)
   end
+
+  def require_login
+    unless session[:logged_in?]
+      flash[:error] = "You must be logged in to view this content"
+      redirect_to login_path
+  end
+
 end
