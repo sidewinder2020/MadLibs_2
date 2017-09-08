e276e5e463742fb11924bc6ce8ba4f2ad8051e89
sidewinder2020 <sdwinder1@gmail.com>
2017-09-08 15:21:44 -0600

    Finalize routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   resources :users, only: [:new, :create, :show] do
-    resources :stories, only: [:new, :create, :index, :destroy, :edit, :update]
+    resources :stories, only: [:new, :create, :destroy, :edit, :update]
   end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :stories, only: [:show]
+  resources :stories, only: [:index, :show]
 end
