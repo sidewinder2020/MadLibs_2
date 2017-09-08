0351f0d7eb40369b230b94d4ec62fad10f6f7e0c
sidewinder2020 <sdwinder1@gmail.com>
2017-09-08 14:19:18 -0600

    Finished General User/Session setup - next it: Authorization general setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:username])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to login_path
   end
 
   def destroy
+    session.clear
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :ratings
   has_many :stories, through :ratings
   has_many :user_stories
   has_many :stories, through :user_stories
+
+  def authenticate
+  end
 end
