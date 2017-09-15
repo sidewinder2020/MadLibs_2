db54dccba10155f593f864bd28c0850e4754c65c
sidewinder2020 <sdwinder1@gmail.com>
2017-09-14 20:50:19 -0600

    fixed login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       session[:logged_in?] = true
       redirect_to user_path(user)
